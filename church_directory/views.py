c732e13301944150469ab4e6afa35180bf11ed1c
Gary Talent <gtalent2@gmail.com>
2016-10-24 19:41:29 -0500

    Serve directory as PDF

--- church_directory/views.py
+++ church_directory/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.template import Context, loader
-from .models import Person
+import weasyprint
+import os
+from .models import Person, PERSON_PICTURE_DIR
+
+def _person_image_fetch(url):
+	prefix = 'image://'
+	if url.startswith(prefix):
+		url = 'file://' + os.path.abspath(url[len(prefix):])
+	return weasyprint.default_url_fetcher(url)
 
 # Create your views here.
 
@@ -11,4 +19,11 @@ def index(rqst):
 	people = Person.objects.all()
 	t = loader.get_template('church_directory/person.html')
 	c = Context({'people': people})
-	return HttpResponse(t.render(c))
+	html_doc = t.render(c)
+	#return HttpResponse(html_doc)
+	pdf_doc = weasyprint.HTML(string=html_doc, url_fetcher=_person_image_fetch).write_pdf()
+
+	response = HttpResponse(pdf_doc, content_type='application/pdf')
+	response['Content-Disposition'] = 'filename="Church_Directory.pdf"'
+
+	return response
