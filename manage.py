09357715313dde934ae3d282c5829e50d8c40c91
Gary Talent <gtalent2@gmail.com>
2016-11-15 00:27:52 -0600

    Make manage.py correctly handle missing SOMA_HOME

--- manage.py
+++ manage.py
@@ -2,9 +2,13 @@
 import os
 import sys
 
-if __name__ == "__main__":
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soma.settings")
-	os.environ.setdefault("SOMA_HOME", "soma_home")
+if __name__ == '__main__':
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soma.settings')
+	os.environ.setdefault('SOMA_HOME', 'soma_home')
+	try:
+		os.mkdir(os.environ['SOMA_HOME'])
+	except OSError:
+		pass
 
 	from django.core.management import execute_from_command_line
 
