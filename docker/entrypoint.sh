93323b4e45071084abdb1a9ec2ed1fb2860a5cd6
Gary Talent <gtalent2@gmail.com>
2017-02-21 23:31:19 -0600

    Fix entrypoint.sh to correctly set the default uid

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,9 +7,11 @@
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
-USER_ID=${LOCAL_USER_ID:-9001}
+if [[ $LOCAL_USER_ID == "" ]]; then
+	USER_ID=-9001
+fi
 
-if [[ $USER_ID != "-9001" ]]; then
+if [[ $USER_ID != -9001 ]]; then
 	if [[ $(id -u user 2> /dev/null) != $USER_ID ]]; then
 		useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 		export HOME=/home/user
