322e90d6aa84a34f0eb81c880abe5b515e4b2bf2
Gary Talent <gtalent2@gmail.com>
2017-05-30 02:04:50 -0500

    Fix for directory PDF call for Django 1.11

--- church_directory/views.py
+++ church_directory/views.py
@@ -3,7 +3,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.template import Context, loader
+from django.template import loader
 import weasyprint
 from datetime import datetime
 from .models import Person, membership_status_str, sex_str, PERSON_PICTURE_DIR
@@ -19,50 +19,50 @@ def _person_image_fetch(url):
 
 @login_required
 def church_directory_pdf(rqst):
-	people = Person.objects.all().order_by('last_name', 'first_name')
-	now = datetime.now()
-	for p in people:
-		p.membership_status = membership_status_str(p.membership_status)
-	t = loader.get_template(SOMA_HOME + '/templates/church_directory.html')
-	c = Context({
-		'people': people,
-		'church_name': CHURCH_NAME,
-		'month_name': now.strftime('%B'),
-		'year': now.year
-	})
-	html_doc = t.render(c)
-	#return HttpResponse(html_doc)
-	pdf_doc = weasyprint.HTML(string=html_doc, url_fetcher=_person_image_fetch).write_pdf()
+    people = Person.objects.all().order_by('last_name', 'first_name')
+    now = datetime.now()
+    for p in people:
+            p.membership_status = membership_status_str(p.membership_status)
+    t = loader.get_template(SOMA_HOME + '/templates/church_directory.html')
+    c = {
+        'people': people,
+        'church_name': CHURCH_NAME,
+        'month_name': now.strftime('%B'),
+        'year': now.year
+    }
+    html_doc = t.render(c)
+    #return HttpResponse(html_doc)
+    pdf_doc = weasyprint.HTML(string=html_doc, url_fetcher=_person_image_fetch).write_pdf()
 
-	response = HttpResponse(pdf_doc, content_type='application/pdf')
-	response['Content-Disposition'] = 'filename="Church_Directory.pdf"'
+    response = HttpResponse(pdf_doc, content_type='application/pdf')
+    response['Content-Disposition'] = 'filename="Church_Directory.pdf"'
 
-	return response
+    return response
 
 @login_required
 def directory_page(rqst):
-	people = Person.objects.all().order_by('last_name', 'first_name')
-	out = []
-	for p in people:
-		out.append({
-			'person_id': p.person_id,
-			'first_name': p.first_name,
-			'middle_name': p.middle_name,
-			'last_name': p.last_name,
-			'suffix': p.suffix,
-			'sex': sex_str(p.sex),
-			'birthday': str(p.birthday),
-			'home_number': p.home_number,
-			'cell_number': p.cell_number,
-			'email_address': p.email_address,
-			'address': {
-				'address_line1': p.address_line1,
-				'address_line2': p.address_line2,
-				'unit_number': p.unit_number,
-			},
-			'membership_status': membership_status_str(p.membership_status),
-			'father': p.father.person_id if p.father else None,
-			'mother': p.mother.person_id if p.mother else None,
-		})
-	response = HttpResponse(json.dumps(out, indent=3), content_type='application/json')
-	return response
+    people = Person.objects.all().order_by('last_name', 'first_name')
+    out = []
+    for p in people:
+        out.append({
+            'person_id': p.person_id,
+            'first_name': p.first_name,
+            'middle_name': p.middle_name,
+            'last_name': p.last_name,
+            'suffix': p.suffix,
+            'sex': sex_str(p.sex),
+            'birthday': str(p.birthday),
+            'home_number': p.home_number,
+            'cell_number': p.cell_number,
+            'email_address': p.email_address,
+            'address': {
+                'address_line1': p.address_line1,
+                'address_line2': p.address_line2,
+                'unit_number': p.unit_number,
+            },
+            'membership_status': membership_status_str(p.membership_status),
+            'father': p.father.person_id if p.father else None,
+            'mother': p.mother.person_id if p.mother else None,
+        })
+    response = HttpResponse(json.dumps(out, indent=3), content_type='application/json')
+    return response
