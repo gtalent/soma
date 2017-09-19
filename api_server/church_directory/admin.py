3ff5685609f2523c152035bef017bfb8c536b0db
Gary Talent <gtalent2@gmail.com>
2017-09-19 18:25:13 -0500

    Add RoleAssignmentAdmin definition

--- api_server/church_directory/admin.py
+++ api_server/church_directory/admin.py
@@ -10,8 +10,11 @@ class PersonAdmin(admin.ModelAdmin):
 	row_name.short_description = 'Name'
 	row_name.admin_order_field = 'last_name'
 
+class RoleAssignmentAdmin(admin.ModelAdmin):
+	list_display = ('person', 'role_type', 'start_date', 'end_date')
+
 admin.site.register(Event)
 admin.site.register(EventType)
 admin.site.register(Person, PersonAdmin)
-admin.site.register(RoleAssignment)
+admin.site.register(RoleAssignment, RoleAssignmentAdmin)
 admin.site.register(Role)
